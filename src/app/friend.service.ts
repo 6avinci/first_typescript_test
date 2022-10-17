a52a66d69596962b0f987d9b31583b9053d001aa
Hauke <hauke.rohen@combridge-itc.de>
2022-10-17 22:01:48 +0200

    ngIf Part 2/2

--- src/app/friend.service.ts
+++ src/app/friend.service.ts
@@ -4,20 +4,13 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class FriendService {
-  names = [
-    'Rose',
-  ];
-  images = [
-    'assets/img/user/6.jpg',
-  ];
-
-
-    descSmall = [
-    'Lebenszeit: ca. 5 Jahre',
-  ]; 
+  names = Array();
+  images = Array();
+  descSmall = Array(); 
 
   constructor() { }
 
+    // addFriend('Peter', 'Hallo', '/assets/img/hamster/1.jpg')
   addFriend(name:string, text:string, image:string) {
     this.names.push (name);
     this.descSmall.push (text);
